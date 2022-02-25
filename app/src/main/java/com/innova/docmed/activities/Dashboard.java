b9ae19d775a9698a8fe70e2371dc90c9a93cc85b
rafay-ah <54492363+rafay-ah@users.noreply.github.com>
2022-02-25 17:25:09 +0500

    Patient Features Completed

--- app/src/main/java/com/innova/docmed/activities/Dashboard.java
+++ app/src/main/java/com/innova/docmed/activities/Dashboard.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -64,5 +65,14 @@ public class Dashboard extends AppCompatActivity {
         Intent k = new Intent(getApplicationContext(), Appointments.class);
         startActivity(k);
 
+    }
+
+    public void signOut(View view) {
+        FirebaseAuth.getInstance().signOut();
+        Toast.makeText(Dashboard.this, "Signing you out!!",
+                Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(getApplicationContext(), Signin.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/innova/docmed/patient/PatientProfile.java
+++ app/src/main/java/com/innova/docmed/patient/PatientProfile.java
@@ -1,16 +1,90 @@
 package com.innova.docmed.patient;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textview.MaterialTextView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.annotations.Nullable;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.innova.docmed.R;
+import com.squareup.picasso.Picasso;
+
+import dmax.dialog.SpotsDialog;
 
 public class PatientProfile extends AppCompatActivity {
+    private MaterialTextView doctorName;
+    private MaterialTextView doctorSpe;
+    private MaterialTextView doctorPhone;
+    private MaterialTextView doctorEmail;
+    private MaterialTextView doctorAddress;
+    private MaterialTextView doctorAbout;
+    private ImageView doctorImage;
+    StorageReference pathReference ;
+    final String doctorID = FirebaseAuth.getInstance().getCurrentUser().getEmail().toString();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    DocumentReference docRef = db.collection("Patient").document("" + doctorID + "");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_profile);
+
+        doctorName = findViewById(R.id.doctor_name);
+        doctorSpe = findViewById(R.id.doctor_specialite);
+        doctorPhone = findViewById(R.id.doctor_phone);
+        doctorEmail = findViewById(R.id.doctor_email);
+        doctorAddress = findViewById(R.id.doctor_address);
+        doctorAbout = findViewById(R.id.doctor_about);
+        doctorImage = findViewById(R.id.imageView3);
+        Drawable defaultImage = getResources().getDrawable(R.drawable.ic_anon_user_48dp); //default user image
+        AlertDialog dialog = new SpotsDialog.Builder().setContext(this).setCancelable(true).build();
+        dialog.show();
+
+        String imageId = FirebaseAuth.getInstance().getCurrentUser().getEmail().toString();
+        pathReference = FirebaseStorage.getInstance().getReference().child("DoctorProfile/"+ imageId+".jpg");
+        pathReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.with(PatientProfile.this)
+                        .load(uri)
+                        .placeholder(R.mipmap.ic_launcher)
+                        .fit()
+                        .centerCrop()
+                        .into(doctorImage);//hna fin kayn Image view
+                dialog.dismiss();
+                // profileImage.setImageURI(uri);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
+
+        docRef.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                doctorName.setText(documentSnapshot.getString("name"));
+                doctorPhone.setText(documentSnapshot.getString("tel"));
+                doctorEmail.setText(documentSnapshot.getString("email"));
+                doctorAddress.setText(documentSnapshot.getString("adresse"));
+                doctorImage.setImageDrawable(defaultImage);
+            }
+        });
     }
 }
